8fbdb16ff675f67cc70217cf791ed82a687ade26
S1monG <si5564gu-s@student.lu.se>
2025-08-01 16:33:52 +0200

    Fix major reset bug and refactor to simplify timing phases

--- onewire_reset.v
+++ onewire_reset.v
@@ -5,31 +5,29 @@ module onewire_reset (
     output reg sample,
     output reg done
 );
-    reg [$clog2(960*27):0] delay_counter = 0;
+    reg [$clog2(960*27):0] delay_counter;
+
+    // Timing parameters
+    localparam DRIVE_TIME = 480 * 27;
+    localparam SAMPLE_POINT = 550 * 27;
+    localparam TOTAL_TIME = 960 * 27;
 
     // Sample bus to determine if a device is present
     // Sample signal is high for one clock cycle, this is when top module should sample onewire
     // 0 = device present, 1 = no device present
     always @(posedge clk) begin
-        if (enable & !done) begin
-            if (delay_counter < 480*27) begin
-                delay_counter <= delay_counter + 1;
-                drive_low <= 1'b1;
-            end else if (delay_counter < 550*27) begin
-                delay_counter <= delay_counter + 1;
-                drive_low <= 1'b0;
-            end else if (delay_counter == 550*27) begin
-                sample <= 1'b1;
-            end else if (delay_counter < 960*27) begin
-                delay_counter <= delay_counter + 1;
-                sample <= 1'b0;
-            end else begin
-                done <= 1'b1;
-                delay_counter <= 0;
-            end
-        end else if (!enable) begin
+        if (!enable) begin
+            delay_counter <= 0;
+            drive_low <= 1'b0;
+            sample <= 1'b0;
             done <= 1'b0;
+        end else if (!done) begin
+            delay_counter <= delay_counter + 1;
+            drive_low <= (delay_counter < DRIVE_TIME);
+            sample <= (delay_counter == SAMPLE_POINT);
+            done <= (delay_counter >= TOTAL_TIME);
         end
+        // If done and enable, hold all current values
     end
 
 endmodule
